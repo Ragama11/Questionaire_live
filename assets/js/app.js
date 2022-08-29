25958767a193eaf151bbf76f1244c6a987c3969e
Ragama11 <87313908+Ragama11@users.noreply.github.com>
2022-08-29 16:24:23 +0300

    bootstrap_added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
-import "../css/app.css"
+// import "../css/app.css"
 import "./admin.js"
 import "./demo.js"
 import "./helpers.js"
@@ -25,6 +25,8 @@ import "./helpers.js"
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
+import "bootstrap"
+
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
